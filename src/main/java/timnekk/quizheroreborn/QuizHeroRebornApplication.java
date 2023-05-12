14114ab6b6572a2542a1dda453b714f1bfb99408
Tim <herew26@gmail.com>
2023-05-12 19:38:08 +0300

    style: move tabs

--- src/main/java/timnekk/quizheroreborn/QuizHeroRebornApplication.java
+++ src/main/java/timnekk/quizheroreborn/QuizHeroRebornApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuizHeroRebornApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizHeroRebornApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuizHeroRebornApplication.class, args);
+    }
 
 }
