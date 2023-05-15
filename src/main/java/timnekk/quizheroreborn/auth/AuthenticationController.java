5737f6cdd722e802e3e1d0d8bc039313ebeff1de
Tim <herew26@gmail.com>
2023-05-15 18:39:29 +0300

    chore: document controllers

--- src/main/java/timnekk/quizheroreborn/auth/AuthenticationController.java
+++ src/main/java/timnekk/quizheroreborn/auth/AuthenticationController.java
@@ -1,5 +1,10 @@
 package timnekk.quizheroreborn.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,22 +14,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import timnekk.quizheroreborn.auth.models.AuthenticationRequest;
+import timnekk.quizheroreborn.auth.models.AuthenticationResponse;
+import timnekk.quizheroreborn.auth.models.RegisterRequest;
+import timnekk.quizheroreborn.exception.model.StringExceptionResponse;
+import timnekk.quizheroreborn.exception.model.StringMapExceptionResponse;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Register new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully registered new user",
+                    content = @Content(schema = @Schema(implementation = AuthenticationResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters",
+                    content = @Content(schema = @Schema(implementation = StringMapExceptionResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Username already exists",
+                    content = @Content(schema = @Schema(implementation = StringExceptionResponse.class)))
+    })
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
         AuthenticationResponse response = authenticationService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Authenticate user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully authenticated user",
+                    content = @Content(schema = @Schema(implementation = AuthenticationResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Invalid credentials",
+                    content = @Content(schema = @Schema(implementation = StringMapExceptionResponse.class)))
+    })
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@Valid @RequestBody AuthenticationRequest request) {
         AuthenticationResponse response = authenticationService.authenticate(request);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
 }
--- src/main/java/timnekk/quizheroreborn/leaderboard/LeaderboardController.java
+++ src/main/java/timnekk/quizheroreborn/leaderboard/LeaderboardController.java
@@ -1,5 +1,10 @@
 package timnekk.quizheroreborn.leaderboard;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -7,6 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import timnekk.quizheroreborn.exception.model.StringExceptionResponse;
+import timnekk.quizheroreborn.exception.model.StringMapExceptionResponse;
+import timnekk.quizheroreborn.leaderboard.models.LeaderboardRequest;
+import timnekk.quizheroreborn.leaderboard.models.LeaderboardResponse;
 
 @RestController
 @RequestMapping("/api/v1/leaderboard")
@@ -15,6 +24,14 @@ public class LeaderboardController {
 
     private final LeaderboardService leaderboardService;
 
+    @Operation(summary = "Get leaderboard")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully fetched the leaderboard"),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters",
+                    content = @Content(schema = @Schema(implementation = StringMapExceptionResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Not authenticated",
+                    content = @Content(schema = @Schema(implementation = StringExceptionResponse.class)))
+    })
     @GetMapping
     public ResponseEntity<LeaderboardResponse> getLeaderboard(@Valid LeaderboardRequest request) {
         LeaderboardResponse response = leaderboardService.getLeaderboard(request);
