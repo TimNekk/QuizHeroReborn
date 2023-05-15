e72e02516e5fb4f540d3cf50cff1b37b1a26fe06
Tim <herew26@gmail.com>
2023-05-16 01:04:11 +0300

    feat: add empty constructor for exceptions

--- src/main/java/timnekk/quizheroreborn/exception/GameSessionDoesNotExistException.java
+++ src/main/java/timnekk/quizheroreborn/exception/GameSessionDoesNotExistException.java
@@ -2,6 +2,10 @@ package timnekk.quizheroreborn.exception;
 
 public class GameSessionDoesNotExistException extends RuntimeException {
 
+    public GameSessionDoesNotExistException() {
+        super();
+    }
+
     public GameSessionDoesNotExistException(String message) {
         super(message);
     }
