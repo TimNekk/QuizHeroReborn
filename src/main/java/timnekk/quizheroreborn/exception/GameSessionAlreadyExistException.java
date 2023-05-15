dd3abe8c4fa005080e8f30fdf1eb479d525c1fdf
Tim <herew26@gmail.com>
2023-05-15 18:38:11 +0300

    feat: add new exceptions

---
+++ src/main/java/timnekk/quizheroreborn/exception/GameSessionAlreadyExistException.java
@@ -0,0 +1,9 @@
+package timnekk.quizheroreborn.exception;
+
+public class GameSessionAlreadyExistException extends RuntimeException{
+
+    public GameSessionAlreadyExistException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/timnekk/quizheroreborn/exception/GameSessionDoesNotExistException.java
@@ -0,0 +1,9 @@
+package timnekk.quizheroreborn.exception;
+
+public class GameSessionDoesNotExistException extends RuntimeException {
+
+    public GameSessionDoesNotExistException(String message) {
+        super(message);
+    }
+
+}
